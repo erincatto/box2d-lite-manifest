e91f4d9a51b58285b0890ab8d87672dd164f62c8
Erin Catto <erincatto@users.noreply.github.com>
2022-10-01 20:18:14 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Box2D-Lite
+Box2D-Lite is a small 2D physics engine. It was developed for the [2006 GDC Physics Tutorial](docs/GDC2006_Catto_Erin_PhysicsTutorial.pdf). This is the original version of the larger [Box2D](https://box2d.org) library. The Lite version is more suitable for learning about game physics.
+
+# Building
+- Install [CMake](https://cmake.org/)
+- Ensure CMake is in the user `PATH`
+- Visual Studio 2017: run `build.bat`
+- Otherwise: run `build.sh` from a bash shell
+- Results are in the build sub-folder
+
+# Build Status
+[![Build Status](https://travis-ci.org/erincatto/box2d-lite.svg?branch=master)](https://travis-ci.org/erincatto/box2d-lite)
---
+++ include/box2d-lite/Body.h
@@ -0,0 +1,40 @@
+/*
+* Copyright (c) 2006-2007 Erin Catto http://www.box2d.org
+*
+* Permission to use, copy, modify, distribute and sell this software
+* and its documentation for any purpose is hereby granted without fee,
+* provided that the above copyright notice appear in all copies.
+* Erin Catto makes no representations about the suitability 
+* of this software for any purpose.  
+* It is provided "as is" without express or implied warranty.
+*/
+
+#pragma once
+
+#include "MathUtils.h"
+
+struct Body
+{
+	Body();
+	void Set(const Vec2& w, float m);
+
+	void AddForce(const Vec2& f)
+	{
+		force += f;
+	}
+
+	Vec2 position;
+	float rotation;
+
+	Vec2 velocity;
+	float angularVelocity;
+
+	Vec2 force;
+	float torque;
+
+	Vec2 width;
+
+	float friction;
+	float mass, invMass;
+	float I, invI;
+};
